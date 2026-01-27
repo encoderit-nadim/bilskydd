0d0222a0f06fbf7b4b5bc4226721ae675dd2979a
encoderit-harun <harun@encoderit.net>
2026-01-27 19:12:18 +0600

    Set font size.

--- frontend/front-page.php
+++ frontend/front-page.php
@@ -9,9 +9,18 @@ get_header();
 <main class="overflow-hidden">
     <section class="section-padding">
         <div class="container">
-            <h1>
-                Main Coming Soon...
+            <h1 class="text_48">
+                Test font size... font size 48
             </h1>
+            <h2 class="text_36">
+                Test font size... font size 36
+            </h2>
+            <h2 class="text_26">
+                Test font size... font size 26
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima molestiae quae quam maiores? Eos quam possimus eveniet quasi autem provident!
+            </p>
         </div>
     </section>
 </main>
